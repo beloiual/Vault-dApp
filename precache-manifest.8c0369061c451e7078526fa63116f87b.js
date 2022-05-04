59b608a2604f32973eb14f88db2f990fe6d57a25
Alexandru Beloiu <beloiual@umich.edu>
2022-05-04 20:21:00 +0000

    Updates

---
+++ precache-manifest.8c0369061c451e7078526fa63116f87b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b228f8c9db436a0a08d1acf6f46bb23f",
+    "url": "/vault-dapp/index.html"
+  },
+  {
+    "revision": "a0c55efb8ee9b9ee88c4",
+    "url": "/vault-dapp/static/js/2.7c483554.chunk.js"
+  },
+  {
+    "revision": "a4f2b9bb8a5e64373ea63996a1af0318",
+    "url": "/vault-dapp/static/js/2.7c483554.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68f37613d1f2979f5d4d",
+    "url": "/vault-dapp/static/js/main.c4ef9b6c.chunk.js"
+  },
+  {
+    "revision": "466b186d3dc6912c46fe",
+    "url": "/vault-dapp/static/js/runtime-main.6e76a654.js"
+  }
+]);
